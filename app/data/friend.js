a6f923db629f4817cb233683575159f6ba0f233e
Jyoti Kumari <jkumari@incomm.com>
2019-05-06 23:07:47 -0400

    Adding code

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Stefanie Ding',
+    profilePic: 'https://image.freepik.com/free-vector/teen-girl-cartoon-character_11460-6830.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
